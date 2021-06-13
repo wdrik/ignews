2d4d4c03e0e2352a816eeec3df56d23d284f39b0
wdrik <w.drik@outlook.com>
2021-06-13 15:58:51 -0300

    feat: add faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,21 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn, useSession } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
   const [session] = useSession();
 
-  function handleClose() {
-    console.log("handleClose");
-  }
-
   return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Iorgen Wildrik
-      <FiX color="#737380" className={styles.closeIcon} onClick={handleClose} />
+      {session.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
     <button
