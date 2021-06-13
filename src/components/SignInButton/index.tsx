dc3517188bc0596366cfa013c7747498162b7f1d
wdrik <w.drik@outlook.com>
2021-06-13 11:36:00 -0300

    feat: add NextAuthProvider

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,28 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
   function handleClose() {
     console.log("handleClose");
   }
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
       Iorgen Wildrik
       <FiX color="#737380" className={styles.closeIcon} onClick={handleClose} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign In With Github
     </button>
