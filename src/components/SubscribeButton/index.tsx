494dab18ca885cb60baf86aaa064e689cf0a6845
wdrik <w.drik@outlook.com>
2021-06-13 21:27:06 -0300

    chore: add axios config

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -8,11 +9,19 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
+    console.log("handleSubscribe");
+
     if (!session) {
       signIn("github");
       return;
     }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+    } catch {}
   }
 
   return (
