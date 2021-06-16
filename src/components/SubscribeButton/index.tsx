471d72c058180726a7f5d95e22ee70b0d3c7d4c9
wdrik <w.drik@outlook.com>
2021-06-16 19:22:28 -0300

    feat: redirect to stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,8 @@
 import { signIn, useSession } from "next-auth/client";
+
 import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -10,8 +13,6 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
   async function handleSubscribe() {
-    console.log("handleSubscribe");
-
     if (!session) {
       signIn("github");
       return;
@@ -21,7 +22,13 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
       const response = await api.post("/subscribe");
 
       const { sessionId } = response.data;
-    } catch {}
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (
