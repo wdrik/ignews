5f33e5f56b7d66b5d37c6792db8b6d6976c4b64a
wdrik <w.drik@outlook.com>
2021-06-20 18:01:36 -0300

    feat: add preview mode

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -43,7 +43,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const session = await getSession({ req });
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: "/",
